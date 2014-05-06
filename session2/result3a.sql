0a060523c0cfe351303ff5fb11b940308551e81f
unknown <m.c.haas@student.utwente.nl>
2014-05-06 16:33:37 +0200

    stap1

---
+++ session2/result3a.sql
@@ -0,0 +1,2 @@
+SELECT m.name AS filmnaam, m.year AS opnamejaar FROM Movie m WHERE m.rat
+ing > (8.6) AND m.rating < (9.1);
---
+++ session2/result3b.sql
@@ -0,0 +1,2 @@
+SELECT a.pid FROM Acts a
+WHERE 'a.mid is the same as the mid from the movie "Back to the Future"';
